99a189bc00735dd8d99e8cca2925f9df9138d299
Edgar Mauricio Gómez Flores <itolisto@gmail.com>
2018-01-12 23:56:15 -0600

    Ui Fixes

--- app/src/main/java/ee/app/conversa/adapters/BusinessAdapter.java
+++ app/src/main/java/ee/app/conversa/adapters/BusinessAdapter.java
@@ -3,6 +3,7 @@ package ee.app.conversa.adapters;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.List;
 import ee.app.conversa.R;
 import ee.app.conversa.holders.BaseHolder;
 import ee.app.conversa.holders.BusinessViewHolder;
+import ee.app.conversa.holders.FixedViewHolder;
 import ee.app.conversa.holders.NHeaderViewHolder;
 import ee.app.conversa.holders.LoaderViewHolder;
 import ee.app.conversa.interfaces.OnContactClickListener;
@@ -25,7 +27,8 @@ public class BusinessAdapter extends RecyclerView.Adapter<BaseHolder> {
 
     private final int HEADER_TYPE = 1;
     private final int BUSINESS_TYPE = 2;
-    private final int LOAD_TYPE = 3;
+    private final int FIXED_TYPE = 3;
+    private final int LOAD_TYPE = 4;
 
     public BusinessAdapter(AppCompatActivity activity, OnContactClickListener localListener) {
         this.mActivity = activity;
@@ -39,7 +42,7 @@ public class BusinessAdapter extends RecyclerView.Adapter<BaseHolder> {
         if (object instanceof nHeaderTitle) {
             return HEADER_TYPE;
         } else if (object instanceof dbBusiness) {
-            return BUSINESS_TYPE;
+            return (((dbBusiness) object).getAvatarVisibility() == View.VISIBLE) ? BUSINESS_TYPE : FIXED_TYPE;
         } else {
             return LOAD_TYPE;
         }
@@ -62,6 +65,11 @@ public class BusinessAdapter extends RecyclerView.Adapter<BaseHolder> {
                     LayoutInflater.from(parent.getContext())
                             .inflate(R.layout.business_item, parent, false),
                     this.mActivity);
+        } else if (viewType == FIXED_TYPE) {
+            return new FixedViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.fixed_item, parent, false),
+                    this.mActivity);
         } else {
             return new LoaderViewHolder(
                     LayoutInflater.from(parent.getContext())
@@ -75,7 +83,11 @@ public class BusinessAdapter extends RecyclerView.Adapter<BaseHolder> {
         Object object = mBusiness.get(i);
 
         if (object.getClass().equals(dbBusiness.class)) {
-            ((BusinessViewHolder) holder).setBusiness(object, localListener);
+            if (((dbBusiness) object).getAvatarVisibility() == View.VISIBLE) {
+                ((BusinessViewHolder) holder).setBusiness(object, localListener);
+            } else {
+                ((FixedViewHolder) holder).setBusiness(object, localListener);
+            }
         } else if (object.getClass().equals(nHeaderTitle.class)) {
             ((NHeaderViewHolder) holder).setHeaderTitle(((nHeaderTitle) object).getHeaderName());
         }
---
+++ app/src/main/java/ee/app/conversa/holders/FixedViewHolder.java
@@ -0,0 +1,36 @@
+package ee.app.conversa.holders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import ee.app.conversa.interfaces.OnContactClickListener;
+import ee.app.conversa.model.database.dbBusiness;
+
+/**
+ * Created by edgargomez on 10/31/16.
+ */
+
+public class FixedViewHolder extends BaseHolder {
+
+    private Object object;
+
+    private OnContactClickListener localListener;
+
+    public FixedViewHolder(View itemView, AppCompatActivity activity) {
+        super(itemView, activity);
+        itemView.setOnClickListener(this);
+    }
+
+    public void setBusiness(Object object, OnContactClickListener localListener) {
+        this.object = object;
+        this.localListener = localListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (localListener != null) {
+            localListener.onContactClick((dbBusiness)object, view, getAdapterPosition());
+        }
+    }
+
+}
